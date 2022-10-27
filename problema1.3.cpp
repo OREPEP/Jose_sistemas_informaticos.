f955c90e8d35725fa65d712783ba756ccf0d93cd
OREPEP <joseantoniogon.alumn@cescristorey.com>
2022-10-27 12:09:46 +0200

    actualizado1.1

--- problema1.3.cpp
+++ problema1.3.cpp
@@ -5,7 +5,7 @@ int main ()
 	const double x = 5 ;
 	const double y = 2 ;
 	cout << "Valor de x " << x <<endl;
-	cout <<"Valor de y"<< y <<endl;
+	cout <<"Valor de bhj y"<< y <<endl;
 	cout <<"valor de x/y\n"<< x/ y ;
 	cout << "\tvalor de y/x" << y / x <<endl;
 }
