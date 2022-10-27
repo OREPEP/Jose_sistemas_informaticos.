d6832ab7ce34ade58b1d9de2afc5effb916d74f2
OREPEP <joseantoniogon.alumn@cescristorey.com>
2022-10-27 11:46:14 +0200

    prueba sistemas

---
+++ problema1.3.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std ;
+int main () 
+{
+	const double x = 5 ;
+	const double y = 2 ;
+	cout << "Valor de x " << x <<endl;
+	cout <<"Valor de y"<< y <<endl;
+	cout <<"valor de x/y\n"<< x/ y ;
+	cout << "\tvalor de y/x" << y / x <<endl;
+}
